7d87a87184ba5a60a4aaef8c4b0f89c77ef11f7a
diegocubillo <dcubillo@comillas.edu>
2024-05-06 18:49:36 +0200

    Leave final md25 plugin with more configuration options and remove variants

--- plugins/md25_plugin.hh
+++ plugins/md25_plugin.hh
@@ -12,7 +12,7 @@ inline namespace IGNITION_GAZEBO_VERSION_NAMESPACE {
 namespace systems
 {
 
-class md25_plugin_o1Private;
+class md25_pluginPrivate;
 
 class md25_motor
 {
@@ -45,7 +45,10 @@ class md25_motor
 
 
 
-  /// \brief Unquantized motor voltage
+  /// \brief Motor voltage expresed in driver register
+  public: int motorVoltRegister = 0;
+
+  /// \brief Motor voltage unquantized
   public: double motorVoltUnquantized = 0.0;
 
   /// \brief Commanded voltage input buffer
@@ -53,6 +56,9 @@ class md25_motor
 
   /// \brief mutex to protect motorVoltCmdBuffer
   public: std::mutex motorVoltCmdBufferMutex;
+
+  /// \brief Objective register commanded for motor voltage
+  public: int motorVoltCmdRegister = 0;
 
   /// \brief Quantized commanded motor voltage
   public: double motorVoltCmdQuantized = 0.0;
@@ -62,10 +68,13 @@ class md25_motor
   public: double prevJointPos = 0.0;
   public: int32_t encoderCount = 0;
 
+  // Last time voltage was changed (in simulation time)
+  public: std::chrono::_V2::steady_clock::duration prevVoltUpdateTime = std::chrono::seconds(0);
+
 
   /// \brief Motor internal variables
   public: double internalCurrent = 0.0;
-  public: double internalOmegaPrev = 0.0;
+  public: double prevInternalOmega = 0.0;
   public: double prevMotorVolt = 0.0;
   
   /// \brief Motor voltage
@@ -75,10 +84,10 @@ class md25_motor
   public: void EncoderSystem(const UpdateInfo &_info, EntityComponentManager &_ecm, const double &_radPerPulse);
 
   /// \brief Motor system
-  public: void MotorSystem(const UpdateInfo &_info, EntityComponentManager &_ecm, md25_plugin_o1Private* _dataPtr,const double &_dt);
+  public: void MotorSystem(const UpdateInfo &_info, EntityComponentManager &_ecm, md25_pluginPrivate* _dataPtr,const double &_dt);
 };
 
-class md25_plugin_o1Private
+class md25_pluginPrivate
 {
   /// \brief Callback for voltage subscription
 
@@ -88,7 +97,6 @@ class md25_plugin_o1Private
 
   /// \brief Battery level (Not integrated with battery plugin yet)
   public: double batteryVoltage = 12.0; // Volts
-  public: double motorNominalVoltage = 12.0; // Volts
   public: double electromotiveForceConstant = 0.539065; // Nm/A
   public: double electricResistance = 7.101; // Ohm
   public: double electricInductance = 0.0034; // Henry
@@ -102,13 +110,16 @@ class md25_plugin_o1Private
   /// \brief Register size from 0 to max voltage
   public: int registerSize = 127;
 
+  /// \brief
+  public: int voltageUpdatePeriod = 25;
+
   /// \brief Maximum voltage step change in 25ms
   public: int maxUpdateSteps = 10;
 
   public: double voltageQuantizationStep; //Volts
 
   // Maximum allowed voltage step in a simulation iteration
-  public: double maxVoltageIncreasePerIter;
+  public: double maxVoltageIncreasePerStep;
 
   // encoder
   // rad per pulse is calculated from sdf value of pulses per revolution
@@ -118,10 +129,13 @@ class md25_plugin_o1Private
   public: int encoderPulsesPerRev = 360;
 
   // Encoder publishing rate [Hz]
-  public: int encoderRate = 10;
+  public: int encoderRate = 100;
 
   // Last time encoder was published (in simulation time)
   public: std::chrono::_V2::steady_clock::duration prevEncoderUpdateTime = std::chrono::seconds(0);
+
+  // Better performance using registers instead of voltage
+  public: bool performanceMode = true;
 
   /// \brief Model interface
   public: Model model{kNullEntity};
@@ -135,20 +149,20 @@ class md25_plugin_o1Private
   private: int ValidateParameters();
 };
 
-class md25_plugin_o1
+class md25_plugin
     : public System, 
       public ISystemPreUpdate, 
       public ISystemConfigure
 {
-  public: md25_plugin_o1();
-  public: ~md25_plugin_o1() override;
+  public: md25_plugin();
+  public: ~md25_plugin() override;
   public: void PreUpdate(const UpdateInfo &_info,
               EntityComponentManager &_ecm) override;
   public: void Configure(const Entity &_entity,
                          const std::shared_ptr<const sdf::Element> &_sdf,
                          EntityComponentManager &_ecm,
                          EventManager &_eventMgr) override;
-  private: std::unique_ptr<md25_plugin_o1Private> dataPtr;
+  private: std::unique_ptr<md25_pluginPrivate> dataPtr;
 };
 
 }
